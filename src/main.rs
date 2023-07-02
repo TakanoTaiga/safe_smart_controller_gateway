be3e46f85474eff72b97719f72f669092b67278e
Taiga Takano <ttttghghnb554z@outlook.jp>
2023-07-02 21:00:51 +0900

    Update
    Signed-off-by: Taiga Takano <ttttghghnb554z@outlook.jp>

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,14 @@
 mod network_module;
 
 use safe_drive::{
-    context::Context, error::DynError, logger::Logger, pr_info, topic::publisher::Publisher
+    //context::Context, 
+    error::DynError, 
+    //logger::Logger, 
+    //pr_info, 
+    //topic::publisher::Publisher
 };
+use async_std::net::UdpSocket;
+use async_std::channel::unbounded;
 
 #[async_std::main]
 async fn main() -> Result<(), DynError> {
@@ -26,11 +32,27 @@ async fn main() -> Result<(), DynError> {
     //let publisher = node.create_publisher::<remote_control_msgs::msg::Button>("my_topic", None)?;
 
     // Create a logger.
-    //let logger = Logger::new("my_talker");
+    //let logger = Logger::new("my_talker"); 
 
-    let udp_task = async_std::task::spawn(network_module::udp_task());
+    let (search_locker_send , search_locker_rcv) = unbounded();
 
-    udp_task.await?;
+    let (target_info_send , target_info_rcv) = unbounded();
+
+    let main_udp_socket = UdpSocket::bind("0.0.0.0:64201").await?;
+    let search_app_socket = UdpSocket::bind("0.0.0.0:0").await?;
+    let ping_socket = UdpSocket::bind("0.0.0.0:0").await?;
+
+    let main_udp_service_task = async_std::task::spawn( 
+        network_module::main_udp_service(main_udp_socket , search_locker_send , target_info_send));
+    let search_app_task = async_std::task::spawn(
+        network_module::search_app(search_app_socket , search_locker_rcv));
+    let ping_task = async_std::task::spawn(
+        network_module::ping_app(ping_socket , target_info_rcv));
+
+    main_udp_service_task.await?;
+    search_app_task.await?;
+    ping_task.await?;
+    
     Ok(())
 }
 
--- src/network_module.rs
+++ src/network_module.rs
@@ -13,28 +13,38 @@
 // limitations under the License.
 
 
-use network_module_util::key::{NodeConnectionKey , ConnectionBuffer , U8KeyUtil};
+use network_module_util::key::{NodeConnectionKey , ConnectionBuffer , U8KeyUtil , EnumKeyUtil};
+use network_module_util::net;
 
 use safe_drive::{error::DynError, logger::Logger, pr_info , pr_error};
+
+use std::os::unix::prelude::OsStringExt;
+use std::time::Duration;
 
 // --- UDP Socket ---
 use async_std::net::UdpSocket;
 use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+use gethostname::gethostname;
 
+// --- Inter Thread Commnunication
+use async_std::channel::{Sender , Receiver};
 
 // --- Get Signal ---
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use signal_hook::flag;
 
-pub async fn udp_task() -> Result<(), DynError> {
+pub async fn main_udp_service(
+    socket: UdpSocket,
+     lock_search: Sender<bool>,
+     ipaddr_send: Sender<SocketAddr>,
+    ) -> Result<(), DynError> {
     // --- Logger ---
     let logger = Logger::new("udp_task");
 
     // --- UDP Socket ---
-    let socket = UdpSocket::bind("0.0.0.0:64201").await?;
     let mut connection_buffer = ConnectionBuffer{
-        connection_key: NodeConnectionKey::MissingKey ,
+        connection_key: NodeConnectionKey::UnknownKey ,
         raw_buffer: [0; 2048] ,
         rcv_size: 0,
         taget_address: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080),
@@ -47,25 +57,32 @@ pub async fn udp_task() -> Result<(), DynError> {
     flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
 
     loop {
+        pr_info!(logger, "loop");
         // --- revive data ---
-        let (recv, addr) = socket.recv_from(&mut connection_buffer.raw_buffer).await?;
+        let (recv, addr) = socket.recv_from(&mut connection_buffer.raw_buffer).await.unwrap();
         connection_buffer.connection_key = connection_buffer.raw_buffer[0].convert_to_enumkey();
         connection_buffer.rcv_size = recv;
         connection_buffer.taget_address.set_ip(addr.ip());
         connection_buffer.taget_address.set_port(64201);
         
+        pr_info!(logger, "Key:{}" , connection_buffer.connection_key);
         match connection_buffer.connection_key {
-            NodeConnectionKey::SearchNode => call_search_node(&logger, &connection_buffer) ,
-            NodeConnectionKey::PingRequest => call_ping_request(&logger, &connection_buffer),
-            NodeConnectionKey::NodeInfoRequest => call_node_info_request(&logger, &connection_buffer),
-            NodeConnectionKey::GamepadValueRequest => call_gamepad_value_request(&logger, &connection_buffer),
-            _ => call_unknown_key(&logger, &connection_buffer),
+            NodeConnectionKey::SearchAppResponse => {
+                lock_search.send(true).await?;
+                ipaddr_send.send(connection_buffer.taget_address).await?;
+            },
+            NodeConnectionKey::PingResponse => {
+                lock_search.send(true).await?;
+            },
+            NodeConnectionKey::GamepadValue => {
+                
+            },
+            _ => {
+                pr_error!(logger, "Unknown ID:{}" , connection_buffer.raw_buffer[0]);
+            },
         }
 
-        // --- send data --- 
-        socket.send_to(&connection_buffer.raw_buffer, &connection_buffer.taget_address).await?;
-
-        // --- Check Signal ---
+        //--- Check Signal ---
         if term.load(Ordering::Relaxed) {
             break;
         }
@@ -73,35 +90,63 @@ pub async fn udp_task() -> Result<(), DynError> {
     Ok(())
 }
 
-fn call_search_node(logger: &Logger , connection_buffer: &ConnectionBuffer){
-    pr_info!(logger, "Key:{}" , connection_buffer.connection_key);
+
+pub async fn search_app(socket: UdpSocket , locker: Receiver<bool> ) -> Result<(), DynError>{
+    let logger = Logger::new("search_app");
+    socket.set_broadcast(true)?;
+
+    // --- create packet ---
+    let mut buffer: Vec<u8> = vec![NodeConnectionKey::SearchApp.convert_to_u8key()];
+    buffer.push(0);
+    buffer.append(&mut net::get_ip());
+    let ip_port: u16 = 64201;
+    buffer.append(&mut ip_port.to_le_bytes().to_vec());
+    buffer.append(&mut gethostname().into_vec());
+
+    buffer.resize(24, 0);
+
+    // --- Get Signal ---
+    let term = Arc::new(AtomicBool::new(false));
+    flag::register(signal_hook::consts::SIGTERM, Arc::clone(&term))?;
+
+    loop {
+        if locker.try_recv() == Ok(true){
+            async_std::task::sleep(Duration::from_millis(1000)).await;
+            continue;
+        }
+        pr_info!(logger, "search_app_task");
+        socket.send_to(&buffer, &SocketAddr::new(IpAddr::V4(Ipv4Addr::new(255, 255, 255, 255)), 64201)).await?;
+        async_std::task::sleep(Duration::from_millis(500)).await; 
+
+        //--- Check Signal ---
+        if term.load(Ordering::Relaxed) {
+            break;
+        }
+    }
+    Ok(())
 }
 
-fn call_ping_request(logger: &Logger , connection_buffer: &ConnectionBuffer){
-    pr_info!(logger, "Key:{}" , connection_buffer.connection_key);
-}
 
-fn call_node_info_request(logger: &Logger , connection_buffer: &ConnectionBuffer){
-    pr_info!(logger, "Key:{}" , connection_buffer.connection_key);
-}
+pub async fn ping_app(socket: UdpSocket , target_info_rcv: Receiver<SocketAddr>) -> Result<(), DynError>{
+    let logger = Logger::new("ping_app");
 
-fn call_gamepad_value_request(logger: &Logger , connection_buffer: &ConnectionBuffer){
-    pr_info!(logger, "Key:{}" , connection_buffer.connection_key);
-}
+    loop {
+        let mut  addr= target_info_rcv.recv().await?;
 
-fn call_unknown_key(logger: &Logger , connection_buffer: &ConnectionBuffer){
-    if let NodeConnectionKey::UnknownKey = connection_buffer.connection_key {
-        pr_error!(logger, "Unknown ID:{}" , connection_buffer.raw_buffer[0]);
-    }else{
-        pr_info!(logger, "Server Key:{}" , connection_buffer.connection_key);
+        loop {
+            let mut buffer: Vec<u8> = vec![NodeConnectionKey::PingRequest.convert_to_u8key()];
+            buffer.push(0);
+            socket.send_to(&buffer, addr).await?;
+            pr_info!(logger, "ping send");
+
+            async_std::task::sleep(Duration::from_millis(500)).await;
+
+            match target_info_rcv.try_recv() {
+                Ok(socket_addr) => {
+                    addr = socket_addr;
+                }
+                Err(_) => {}
+            } 
+        }
     }
 }
-
-
-
-// gomi
-
-// let buf = &mut connection_buffer.raw_buffer[..recv];
-// let sent = socket.send_to(&buf[..recv], &peer).await?;
-// pr_info!(logger, "size: {:?}", recv);
-// pr_info!(logger, "message: {:?}", buf[0]);
