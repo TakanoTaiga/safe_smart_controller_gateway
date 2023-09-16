2f5cb1357f9f04fb2aa3242a7f47eaf970f4102a
Taiga Takano <ttttghghnb554z@outlook.jp>
2023-09-16 12:29:15 +0900

    Add ID
    Signed-off-by: Taiga Takano <ttttghghnb554z@outlook.jp>

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,10 @@
 
 mod network_module;
 
-use safe_drive::{
-    context::Context, 
-    error::DynError, 
-    logger::Logger,
-    pr_info,
-};
-use async_std::net::UdpSocket;
 use async_std::channel::unbounded;
-use ros2_rust_util::{get_i64_parameter, get_bool_parameter};
-
+use async_std::net::UdpSocket;
+use ros2_rust_util::{get_bool_parameter, get_i64_parameter};
+use safe_drive::{context::Context, error::DynError, logger::Logger, pr_info};
 
 #[async_std::main]
 async fn main() -> Result<(), DynError> {
@@ -35,28 +29,44 @@ async fn main() -> Result<(), DynError> {
 
     // Create a logger.
     let logger = Logger::new("scgw");
-    pr_info!(logger, "Start {:?}",node.get_name());
+    pr_info!(logger, "Start {:?}", node.get_name());
 
     let param_port = get_i64_parameter(node.get_name(), "port", 64201);
     let param_debug = get_bool_parameter(node.get_name(), "debug", false);
-    
-    let (closer_send , closer_rcv) = unbounded();
+    let param_id = get_i64_parameter(node.get_name(), "id", 120);
 
-    let (search_locker_send , search_locker_rcv) = unbounded();
-    let (target_info_send , target_info_rcv) = unbounded();
+    let (closer_send, closer_rcv) = unbounded();
+
+    let (search_locker_send, search_locker_rcv) = unbounded();
+    let (target_info_send, target_info_rcv) = unbounded();
 
     let main_udp_socket = UdpSocket::bind(format!("0.0.0.0:{param_port}")).await?;
     let search_app_socket = UdpSocket::bind("0.0.0.0:0").await?;
     let ping_socket = UdpSocket::bind("0.0.0.0:0").await?;
-    
-    let get_signaler = async_std::task::spawn(
-        network_module::get_signal(closer_send));
-    let main_udp_service_task = async_std::task::spawn( 
-        network_module::main_udp_service( main_udp_socket, closer_rcv.clone(), search_locker_send, target_info_send, publisher, param_debug.clone()));
-    let search_app_task = async_std::task::spawn(
-        network_module::search_app( search_app_socket ,closer_rcv.clone(), search_locker_rcv, param_debug.clone()));
-    let ping_task = async_std::task::spawn(
-        network_module::ping_app( ping_socket ,closer_rcv.clone() , target_info_rcv , param_debug.clone()));
+
+    let get_signaler = async_std::task::spawn(network_module::get_signal(closer_send));
+    let main_udp_service_task = async_std::task::spawn(network_module::main_udp_service(
+        main_udp_socket,
+        closer_rcv.clone(),
+        search_locker_send,
+        target_info_send,
+        publisher,
+        param_debug.clone(),
+    ));
+    let search_app_task = async_std::task::spawn(network_module::search_app(
+        search_app_socket,
+        closer_rcv.clone(),
+        search_locker_rcv,
+        param_id.clone(),
+        param_debug.clone(),
+    ));
+    let ping_task = async_std::task::spawn(network_module::ping_app(
+        ping_socket,
+        closer_rcv.clone(),
+        target_info_rcv,
+        param_id.clone(),
+        param_debug.clone(),
+    ));
 
     main_udp_service_task.await?;
     search_app_task.await?;
@@ -66,4 +76,3 @@ async fn main() -> Result<(), DynError> {
     pr_info!(logger, "End node");
     Ok(())
 }
-
