da47b48950a2e9ac5ffb43c4a04e3805b17a4eec
Taiga Takano <ttttghghnb554z@outlook.jp>
2023-12-31 19:46:14 +0900

    Add lib module

--- src/network_module_util.rs
+++ src/network_module_util.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 Taiga Takano
+// Copyright 2023 Hakoroboken
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,5 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod network_module;
-pub mod network_module_util;
+
+pub mod key;
+pub mod data;
+pub mod net;
